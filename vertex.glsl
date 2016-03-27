90146ea895ba7aae326477b73da894bfd0c9491c
Will Greenberg <will@endlessm.com>
2016-03-26 21:20:54 -0700

    :scream:

--- vertex.glsl
+++ vertex.glsl
@@ -1,3 +1,5 @@
+precision mediump int;
+uniform int u_time;
 attribute vec3 a_position;
 uniform mat4 u_model;
 uniform mat4 u_view;
@@ -7,5 +9,7 @@ varying vec3 a;
 
 void main() {
     a = a_position;
-    gl_Position = u_proj * u_view * u_model * vec4(a_position, 1.0);
+    mat4 wtf = mat4(1.0);
+    wtf[2][3] = abs(sin(float(u_time)/60.0) * 5.0);
+    gl_Position = wtf * u_proj * u_view * u_model * vec4(a_position, 1.0);
 }
