54d94a37d7e4b96216dbbc20c554c83e8b6ae2c0
Will Greenberg <will@endlessm.com>
2016-03-28 22:29:07 -0700

    more :sparkles:

--- fragment.glsl
+++ fragment.glsl
@@ -1,16 +1,23 @@
 precision mediump float;
 uniform int u_time;
 varying vec3 a;
+varying mat4 m;
 
 float margin = 0.1;
 float PI = 3.14;
 
 // pretty colors
-vec3 rainbow (int t) {
-    t /= 2; // up dat frequency
-    float r = sin(float(t) * (PI/180.0));
-    float g = cos(float(t) * (PI/180.0) + 1.0);
-    float b = sin(float(t) * (PI/180.0) + 2.0);
+vec3 rainbow (float t) {
+    // hsl-ish via
+    // http://www.quasimondo.com/archives/000696.php#000696
+    t = t * (PI / 180.0);
+    float saturation = 0.8;
+    float luminance = 0.5;
+    float u = cos(t) * saturation;
+    float v = sin(t) * saturation;
+    float r =  luminance  + 1.139837398373983740  * v;
+    float g = luminance  - 0.3946517043589703515  * u - 0.5805986066674976801 * v;
+    float b = luminance + 2.03211091743119266 * u;
     return vec3(r,g,b);
 }
 
@@ -20,26 +27,20 @@ void main() {
     bool y_margin = a.y < -1.0 + margin || a.y > 1.0 - margin;
     bool z_margin = a.z < -1.0 + margin || a.z > 1.0 - margin;
     bool ns_edge = a.z == 1.0 || a.z == -1.0;
+    float zt = m[3][2];
+    float xt = m[3][0];
     vec3 color;
     if (ns_edge) {
         if (x_margin || y_margin) {
             color = vec3(0.0);
         } else {
-            // crazy rainbow shit
-            if (sin(a.y * 10.0 + float(u_time)/10.0) > 0.0)
-                color = vec3(rainbow(u_time));
-            else
-                color = vec3(0.9);
+            color = rainbow(float(u_time) + zt - xt);
         }
     } else {
         if (z_margin || y_margin) {
             color = vec3(0.0);
         } else {
-            // crazy rainbow shit
-            if (sin(a.y * 10.0 + float(u_time)/10.0) > 0.0)
-                color = vec3(rainbow(u_time));
-            else
-                color = vec3(0.9);
+            color = rainbow(float(u_time) + zt - xt);
         }
     }
     gl_FragColor = vec4(color, 1.0);
--- vertex.glsl
+++ vertex.glsl
@@ -6,11 +6,15 @@ uniform mat4 u_view;
 uniform mat4 u_proj;
 
 varying vec3 a;
+varying mat4 m;
 
 void main() {
     a = a_position;
+    m = u_model;
     mat4 wtf = mat4(1.0);
-    wtf[3][1] = sin(float(u_time) / 60.0 + u_model[3][2]/10.0) / 1.5;
-    wtf[3][1] += sin(float(u_time) / 60.0 + u_model[3][0]/10.0) / 1.5;
+    float zt = u_model[3][2];
+    float xt = u_model[3][0];
+    wtf[3][1] = sin(float(u_time) / 100.0 + zt/10.0) / 2.5;
+    wtf[3][1] += sin(float(u_time) / 100.0 + xt/10.0) / 2.5;
     gl_Position = u_proj * u_view * u_model * wtf * vec4(a_position, 1.0);
 }
