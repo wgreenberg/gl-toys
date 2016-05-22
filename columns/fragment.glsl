269cf78608b532f48cd7153a58ca0fc318c8f713
Will Greenberg <will@endlessm.com>
2016-05-22 00:36:30 -0700

    wip columns

--- columns/fragment.glsl
+++ columns/fragment.glsl
@@ -8,9 +8,13 @@ varying vec2 v_screenPos;
 #define MAX_DIST 20.0
 #define EPSILON 0.001
 
-#define FREQ 20.0
-#define SINT sin(float(u_time) / 20.0)
-#define COST cos(float(u_time) / 20.0)
+#define FREQ 100.0
+#define SINT sin(float(u_time) / FREQ)
+#define COST cos(float(u_time) / FREQ)
+
+#define NUM_DIVOTS 30
+#define PI 3.14
+#define TAU 2.0*PI
 
 float sphere (vec3 pos, vec3 ctr, float radius) {
     return distance(pos, ctr) - radius;
@@ -20,24 +24,39 @@ float box (vec3 pos, vec3 dim, vec3 ctr) {
     return length(max(abs(pos) - dim, 0.0));
 }
 
-float plane (vec3 pos, vec3 norm, vec3 ctr) {
-    return dot(pos, norm) + length(ctr);
+float plane (vec3 pos, float y, int up) {
+    if (up == 1)
+        return pos.y - y;
+    else
+        return y - pos.y;
+}
+
+float cylinder (vec3 pos, vec3 ctr, float radius, float height) {
+    float d = distance(pos.xz, ctr.xz) - radius;
+    d = max(d, plane(pos, ctr.y + height/2.0, 1));
+    d = max(d, plane(pos, ctr.y - height/2.0, 0));
+    return d;
+}
+
+float column (vec3 pos, vec3 ctr, float radius, float height) {
+    float pillar = cylinder(pos, ctr, radius, height);
+    if (pillar >= 0.5) return pillar;
+    radius += 0.05;
+    float dist = pillar;
+    for (int i=0; i < NUM_DIVOTS; i++) {
+        float rad = TAU * (float(i) / float(NUM_DIVOTS));
+        vec3 cut_ctr = vec3(ctr.x + sin(rad) * radius, ctr.y, ctr.z + cos(rad) * radius);
+        float cut = cylinder(pos, cut_ctr, radius/10.0, height);
+        dist = max(dist, -cut);
+    }
+    return dist;
 }
 
 float dist_field (vec3 pos) {
     float dist = 1.0;
-    vec3 sphereCtr = vec3(1.0, 1.0, 0.0);
-    float sphereRad = 1.0;
-    vec3 boxDim = vec3(1.0, 1.0, 1.0);
-    vec3 boxCtr = vec3(0.0, 0.0, 0.0);
-    vec3 planeNorm = vec3(0.0, 1.0, 0.0);
-    vec3 planeCtr = vec3(0.0, 0.0, -1.0);
-    dist = min(dist, sphere(pos, sphereCtr, sphereRad));
-    dist = min(dist, sphere(pos, vec3(sphereCtr.x, sphereCtr.y + 1.0, sphereCtr.z + 0.5), 0.3));
-    dist = min(dist, sphere(pos, vec3(sphereCtr.x, sphereCtr.y + 1.0, sphereCtr.z - 0.5), 0.3));
-    dist = min(dist, sphere(pos, sphereCtr, sphereRad));
-    dist = min(dist, box(pos, boxDim, boxCtr));
-    dist = min(dist, plane(pos, planeNorm, planeCtr));
+    dist = min(dist, column(pos, vec3(0.0, -1.0, 0.0), 1.0, 0.99));
+    dist = min(dist, column(pos, vec3(0.0, 0.0, 0.0), 1.0, 0.99));
+    dist = min(dist, column(pos, vec3(0.0, 1.0, 0.0), 1.0, 0.99));
     return dist;
 }
 
@@ -50,7 +69,7 @@ vec3 norm_field (vec3 pos) {
 }
 
 void main() {
-    vec3 cam_origin = vec3(SINT * 5.0, 0.0, COST * 5.0);
+    vec3 cam_origin = vec3(SINT*4.0, 0.0, COST*4.0);
     vec3 cam_target = vec3(0.0, 0.0, 0.0);
     vec3 up_dir = vec3(0.0, 1.0, 0.0);
     vec3 cam_dir = normalize(cam_target - cam_origin);
@@ -72,8 +91,7 @@ void main() {
     if (dist < EPSILON) {
         vec3 pos_norm = norm_field(pos);
         float diffuse = max(0.0, dot(-ray_dir, pos_norm));
-        float specular = pow(diffuse, 32.0);
-        gl_FragColor = vec4(vec3(diffuse + specular), 1.0);
+        gl_FragColor = vec4(vec3(diffuse), 1.0);
     } else {
         gl_FragColor = vec4(vec3(1.0), 1.0);
     }
